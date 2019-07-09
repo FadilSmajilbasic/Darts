9d67668c2c81ca285bbdab3103aa596d45b4abdd
Fadil Smajilbasic <fadil.smajilbasic@samtrevano.ch>
2019-07-09 14:43:33 +0100

    Removed fullscreen mode

--- app/src/main/java/com/duck/darts/MainActivity.java
+++ app/src/main/java/com/duck/darts/MainActivity.java
@@ -62,16 +62,6 @@ public class MainActivity extends AppCompatActivity implements View.OnKeyListene
 
         initComponents();
 
-        View decorView = getWindow().getDecorView();
-
-        decorView.setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_IMMERSIVE
-                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_FULLSCREEN);
-
     }
 
     private void initComponents() {
